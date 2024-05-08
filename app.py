92bdc3f319d5696263c99a55e30f92a9003cfec6
dario <887919@stud.unive.it>
2024-05-08 13:46:05 +0200

    aggiunti file html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask import request, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/admin-login')
+def login_admin():
+    return render_template('admin-login.html')
