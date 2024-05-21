b09fb97265742c9517f686277afc4001245ad9e8
Zenny0ne <891000@stud.unie.it>
2024-05-21 17:33:39 +0200

    sex update 3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+import route
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import *
 from flask_sqlalchemy import SQLAlchemy
+from requests import auth
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager
 import re
@@ -10,7 +12,7 @@ app.debug = True
 
 app.config['SECRET_KEY'] = 'ubersecret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:leonardo@localhost:5432/e_commerce'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5433/e_commerce'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -28,8 +30,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(80))
-    password = db.Column(db.String(1000))
-    address = db.Column(db.String(80))
+    password = db.Column(db.String(80))
     role = db.Column(db.String(80))
 
     def set_password(self, password):
@@ -60,27 +61,21 @@ def login():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
-        user = db.session.query(User).filter_by(email=email).first()
+        user = db.session.query(User).filter_by(email=email, password=password).first()
         remember = True if request.form.get('remember') else False
 
+        if email == "admin@admin" and password == "pass123":
+            return redirect('/admin')
+
         if user is not None:
-
-            if user.role == 'admin' and password == 'pass123' and email == "admin@admin":
-                login_user(user, remember=remember)
-                return redirect(url_for('admin'))
-
-            if user.check_password(password):
+            if email == user.email or password == user.password:
                 login_user(user, remember=remember)
                 return redirect(url_for('index'))
-            else:
-                error = 'Invalid credentials'
-                return internal_error(error)
         else:
-            error = 'User not found'
+            error = 'Invalid credentials'
             return internal_error(error)
 
     return render_template('login.html')
-
 
 
 @app.errorhandler(500)
@@ -110,15 +105,28 @@ def register():
         name = request.form['name']
         email_register = request.form['email']
         password = request.form['password']
-        address = request.form['address']
         role = request.form.get('select')
-        users = User(name=name, email=email_register, address=address, role=role)
-        users.set_password(password)
+        users = User(name=name, email=email_register, password=password, role=role)
         db.session.add(users)
         db.session.commit()
         return redirect('/login')
     return render_template('register.html')
 
+
+@app.route('/admin_login', methods=['GET', 'POST'])
+def login_admin():
+    error = None
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+
+        if email == "admin@admin" and password == "pass123":
+            return redirect('/admin')
+        else:
+            error = 'Invalid credentials'
+            return internal_error(error)
+
+    return render_template('admin_login.html')
 
 
 @app.route('/product')
@@ -134,24 +142,9 @@ def cart():
 @app.route('/admin')
 @login_required
 def admin():
-    return render_template('backend/index.html')
+    return render_template('admin.html')
 
 
 @app.route('/products')
 def products():
     return render_template('products.html')
-@app.route('/items_selled')
-def items_selled():
-    return render_template('backend/items_selled.html')
-
-
-@app.route('/inventory')
-@login_required
-def inventory():
-    return render_template('backend/products.html')
-
-
-@app.route('/users')
-def users():
-    user = User.query.all()
-    return render_template('backend/users.html', user=user)
