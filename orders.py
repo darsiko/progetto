5a6d5eec9ae44a12586b07740ff8745426d58b46
Zenny0ne <891000@stud.unie.it>
2024-07-03 15:28:41 +0200

    aggiunte altre robe

--- orders.py
+++ orders.py
@@ -1,10 +1,7 @@
 import datetime
-from flask import Blueprint, render_template, redirect, url_for, session
-from flask_login import login_required, current_user
-from sqlalchemy import func
-from sqlalchemy.orm import aliased
-
-from models import Order, Cart, CartItem, Product
+from flask import Blueprint, render_template, redirect, url_for, session, request
+from flask_login import current_user
+from models import Order, Cart, CartItem, Product, db
 
 orders_blueprint = Blueprint('orders_blueprint', __name__, template_folder='templates', static_folder='static')
 
@@ -15,7 +12,18 @@ def orders():
     return render_template('orders.html', orders=order)
 
 
-@orders_blueprint.route('/orders/<int:idx>', methods=["GET", "POST"])
-def add_order(idx):
+@orders_blueprint.route('/orders', methods=["POST"])
+def add_order():
+    products_id = request.form.getlist('product_id')
+    names = request.form.getlist('name')
+    amounts = request.form.getlist('quantity')
+    prices = request.form.getlist('price')
+
+    cart = [{"product_id": products_id, "name": names, "amount": int(amounts), "price": float(prices)} for products_id, names, amounts, prices in zip(products_id, names, amounts, prices)]
+
+    for item in cart:
+        order = Order(date=datetime.date.today(), state="ordinato", total=item["amount"]*item["price"], user_id=current_user.id, product_id=item["product_id"], quantity=item["amount"])
+        db.session.add(order)
+        db.session.commit()
 
     return redirect(url_for('orders_blueprint.orders'))
--- quary/trigger.sql
+++ quary/trigger.sql
@@ -1,13 +1,19 @@
-create or replace function fun_1()
-returns trigger as $$
-    begin
-        update product
-        set amount = amount - new.quantity
-        where id = new.product_id;
-    end;
-$$ language plpgsql;
+CREATE OR REPLACE FUNCTION update_product_amount() RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT amount FROM Product WHERE id = NEW.product_id) < NEW.quantity THEN
+        RAISE EXCEPTION 'Quantità insufficiente per il prodotto %', NEW.product_id;
+    END IF;
 
-create or replace trigger trig_1
-after insert on "order"
-for each statement
-execute function fun_1();
+    UPDATE Product
+    SET amount = amount - NEW.quantity
+    WHERE id = NEW.product_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON "order"
+FOR EACH ROW
+EXECUTE FUNCTION update_product_amount();
+
