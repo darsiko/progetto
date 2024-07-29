2ec3649a61be711990043c889281be35ae1a0fad
Zenny0ne <zanatta.leonardo02@gmail.com>
2024-07-29 17:02:25 +0200

    Aggiunte robe

--- orders.py
+++ orders.py
@@ -2,7 +2,7 @@ import datetime
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 
-from models import Order, Product, db
+from models import Order, Product, db, Cart, CartItem
 
 orders_blueprint = Blueprint('orders_blueprint', __name__, template_folder='templates', static_folder='static')
 
@@ -37,11 +37,17 @@ def add_order():
     cart = [{"product_id": products_id, "name": names, "amount": int(amounts), "price": float(prices)} for
             products_id, names, amounts, prices in zip(products_id, names, amounts, prices)]
 
+    current_cart = Cart.query.filter_by(user_id=current_user.id).first()
+
     for item in cart:
         order = Order(date=datetime.date.today(), state="ordinato", total=item["amount"] * item["price"],
                       user_id=current_user.id, product_id=item["product_id"], quantity=item["amount"])
         db.session.add(order)
+        cart_product = CartItem.query.filter_by(cart_id=current_cart.id, product_id=item["product_id"]).first()
+        db.session.delete(cart_product)
         db.session.commit()
+
+
 
     return redirect(url_for('orders_blueprint.orders'))
 
