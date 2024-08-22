14bea82daf802b25c1f176e22a43dae138a75a88
Zenny0ne <zanatta.leonardo02@gmail.com>
2024-08-22 14:34:00 +0200

    Aggiunte robe

--- error.py
+++ error.py
@@ -4,10 +4,10 @@ error_pages_blueprint = Blueprint("error_pages", __name__, template_folder="temp
 
 
 @error_pages_blueprint.app_errorhandler(404)
-def error_404(error):
+def error_404():
     return render_template("404.html"), 404
 
 
 @error_pages_blueprint.app_errorhandler(403)
-def error_403(error):
+def error_403():
     return render_template("403.html"), 403
